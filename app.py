c4bf6b714d43f67050b77087c762a69e21ca31cb
shabaz814 <88234413+shabaz814@users.noreply.github.com>
2021-07-31 16:59:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask,jsonify
 import paho.mqtt.client as mqtt
-import json
 import datetime
 message={"devmac":"","EPC":"","Formatted UTC time":""}
 def on_connect(client,userdata,flags,rc):
@@ -10,7 +9,7 @@ def on_message(client,userdata,msg):
     global message
     my_str=str(msg.payload)
     my_str=my_str[2:len(my_str)-1]
-    json_data=json.loads(my_str)
+    json_data=dict(my_str)
     message["devmac"]=json_data["devmac"]
     message["EPC"]=json_data["reads"][0]["EPC"]
     message["Formatted UTC time"]=datetime.datetime.now()
